b6f62c8d421be5bc191ab0e80b386d0f62fbb1b6
tiny_bear <rebeccawilliams9941@gmail.com>
2023-08-22 16:01:29 +1000

    :tada: Start Chatty Potter!

---
+++ chatty_potter.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+  st.title("Chatty Potter ⚡️")
+  st.write("A chatbot built with ChatGPT (3.5 Turbo) where you can pick a Harry Potter character and chat to them as if you were talking over text. My absolute FAVOURITE thing is the emojis that the characters respond with. It is utterly delightful when Voldy comes back with snake and death eater emojis 🐍💀")
+
+if __name__ == "__main__":
+  main()
